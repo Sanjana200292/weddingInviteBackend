869adcda43f8ca8452884f7a025aaab87b0c9596
Sanjana Deshan <sdeshan960@gmail.com>
2025-03-30 00:14:47 +0530

    change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://wedding-invite-frontend.vercel.app/", credentials: true }));
 app.use(cookieParser());
 
 mongoose
